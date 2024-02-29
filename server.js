55bae6eb075bcb5090e2cc5ac2f76016ecc7ce0d
Gedaias Jair <gedaias@geda.dev>
2024-02-29 16:41:20 -0600

    Create server
    Add post method at /send-email endpoint to send the email with nodemailer

---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const nodemailer = require('nodemailer');
+require('dotenv').config();
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+const transporter = nodemailer.createTransport({
+	service: process.env.SERVICE,
+	auth: {
+		user: process.env.USER,
+		pass: process.env.PASS,
+	},
+});
+
+app.get('/index', (req, res) => {
+	res.send('Waiting for request...');
+})
+
+app.post('/send-email', (req, res) => {
+	const {name, email, message} = req.body;
+	const subject = `New message from ${name} <${email}>`;
+	const mailOptions = {
+		from: process.env.USER,
+		to: 'hello@geda.dev',
+		subject,
+		message,
+	};
+
+	transporter.sendMail(mailOptions, (error, info) => {
+		if (error) {
+			console.log(error);
+			res.status(500).send('Error sending email');
+		} else {
+			console.log(`Email sent: ${info.response}`);
+			res.send('Email sent successfully');
+		}
+	});
+});
+
+app.listen(3000, () => {
+	console.log('Server running on port 3000');
+});
